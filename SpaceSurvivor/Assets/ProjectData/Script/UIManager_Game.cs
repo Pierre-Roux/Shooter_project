9656bdbd2fcdafe6d53168d4177a8e7962e2f7a7
Pierre <roux_pierre@outlook.fr>
2024-09-03 02:32:57 +0200

    ExitButton

---
+++ SpaceSurvivor/Assets/ProjectData/Script/UIManager_Game.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    [HideInInspector] public GameObject Player;
+    public GameObject endPanel;
+
+    void Start()
+    {
+        Player = Player_controler.Instance.gameObject; 
+    }
+
+    void Update()
+    {
+        if (Player.GetComponent<Player_controler>().playerMort && !endPanel.activeSelf)
+        {
+            StartCoroutine(DeathUI());
+        }
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f; // Unpause the game
+        SceneManager.LoadScene("Main_Menu"); // Replace with your main menu scene name
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f; // Unpause the game
+        SceneManager.LoadScene("Persitant_Scene"); // Reload the current scene
+    }
+
+    IEnumerator DeathUI()
+    {
+        yield return new WaitForSeconds(3f);
+        endPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
